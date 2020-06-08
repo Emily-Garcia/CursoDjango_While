90566ff2c98fdac449dda8a06e4ee41473ee2a9f
Emily Garcia <garcia.16.emily@gmail.com>
2020-06-08 12:29:03 -0500

    Reto 1 ejercicios Listas

---
+++ abecedario.py
@@ -0,0 +1,5 @@
+abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+word = abc[15].upper() + abc[-2] + abc[-7] + abc[7] + abc[14] + abc[13]
+
+print(word)
